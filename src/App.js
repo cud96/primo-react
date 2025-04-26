630b80d23fb047a287a835566e6011530a7493bf
alessandro <frulla96@gmail.com>
2025-04-26 13:38:49 +0200

    x

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import react from 'react';
 import NavBar from './components/NavBar';
 import Clocks from './components/Clock';
+import Card from './card';
 
 function App() {
   return (
   <div>
-  <NavBar/>
-  <Clocks/>
-  </div>
+    <div>
+    <NavBar/>
+    </div>
+    <Clocks/>
+    <div className='grid grid-cols-4 gap-10'>
+      <Card />
+    </div>
+    
+    </div>
   );
 }
 
